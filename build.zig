10d994f016be7f17e1c6ce9fb10828dde763505f
EliSauder <24995216+EliSauder@users.noreply.github.com>
2024-07-22 18:39:32 -0700

    feat: update build.zig to use the updated api

--- build.zig
+++ build.zig
@@ -4,12 +4,10 @@ const zine = @import("zine");
 pub fn build(b: *std.Build) !void {
     // zine.scriptyReferenceDocs(b, "content/documentation/scripty/index.md");
     try zine.addWebsite(b, .{
+        .title = "Zig Embedded Group",
+        .host_url = "https://microzig.tech",
         .layouts_dir_path = "layouts",
         .content_dir_path = "content",
         .static_dir_path = "static",
-        .site = .{
-            .base_url = "https://microzig.tech",
-            .title = "Zig Embedded Group",
-        },
     });
 }
